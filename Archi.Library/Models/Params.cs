0004bee72fd5c9133a63c2f0db387d833709c226
Melvin Debot <melvin@exceenis.com>
2022-01-20 15:53:50 +0100

    test: add file on library

---
+++ Archi.Library/Models/Params.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archi.Library.Models
+{
+    public class Params
+    {
+        public string Asc { get; set; }
+        public string Desc { get; set; }
+        public string Range { get; set; }
+        public bool HasOrder()
+        {
+            return !string.IsNullOrWhiteSpace(Asc) || !string.IsNullOrWhiteSpace(Desc);
+        }
+        public bool HasRange()
+        {
+            return !string.IsNullOrWhiteSpace(Range);
+        }
+
+    }
+}
